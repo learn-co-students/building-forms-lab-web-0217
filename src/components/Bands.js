8404254bbb469cbacb82b0fed3c33c210c1a84e2
hwalborn <hwalborn@gmail.com>
2017-04-12 10:55:14 -0400

    I miss the cat

--- src/components/Bands.js
+++ src/components/Bands.js
@@ -2,9 +2,12 @@ import React, { Component } from 'react';
 
 class Bands extends Component {
   render(){
+    let bandList = this.props.store.getState().bands.map(band => <li>{band}</li>)
     return (
       <div>
-        band input
+        <ul>
+          {bandList}
+        </ul>
       </div>
     );
   }
