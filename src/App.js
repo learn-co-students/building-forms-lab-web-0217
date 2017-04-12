8d91f1281b1778ad70cec0a9d263cccf64d1064a
johnshelb <jsnd11@gmail.com>
2017-04-12 17:19:26 -0400

    Done.

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <BandInput store={this.props.store} />
+      <BandInput store={this.props.store} />
         <Bands store={this.props.store}/>
       </div>
     );
